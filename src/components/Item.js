2d2a57a4710891e50f98b5b9ec0e1296daf54690
Daniel Merys <dkmerys@gmail.com>
2020-07-01 11:51:12 -0700

    fix image display

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function Item(props) {
   return (
     <React.Fragment>
-      <img src={props.path} />
+      <img src={props.path} alt="merch"/>
       <h3>{props.name}</h3>
       <h5>{props.description}</h5>
       <h4>{props.price}</h4>
@@ -14,6 +14,7 @@ function Item(props) {
 }
 
 Item.propTypes = {
+  path: PropTypes.string,
   name: PropTypes.string,
   description: PropTypes.string,
   price: PropTypes.string,
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,16 +1,61 @@
 import React from "react";
 import Item from "./Item";
 import PropTypes from "prop-types";
+import Shirt from "../img/Shirt.png";
+import Hoodie from "../img/Hoodie.jpg";
+import Socks from "../img/Socks.jpeg";
+import Vinyl from "../img/Vinyl.jpg";
+
+const standardItemList = [
+  {
+    path: Shirt,
+    name: 'Magic Shirt',
+    description: 'A shirt. With the Magic Sword logo on it.',
+    price: '$29',
+    quantity: 25
+  },
+  {
+    path: Hoodie,
+    name: 'Magic Hoodie',
+    description: 'A hoodie. With the Magic Sword logo on it. Also, magic.',
+    price: '$49',
+    quantity: 50
+  },
+  {
+    path: Socks,
+    name: 'Magic Socks',
+    description: 'A rad pair of socks. They have swords on them.',
+    price: '$20',
+    quantity: 35
+  },
+  {
+    path: Vinyl,
+    name: 'Endless Vinyl',
+    description: 'Put that music in your ears!',
+    price: '$20',
+    quantity: 40
+  }
+]
 
 function ItemList(props) {
   return (
     <React.Fragment>
-      {props.itemList.map((item, index) =>
-        <Ticket name={item.name}
+      {standardItemList.map((item, index) =>
+        <Item path={item.path}
+          name={item.name}
           description={item.description}
           price={item.price}
-          quantity={item.quantity} />
+          quantity={item.quantity}
+          key={index} />
       )}
+      {/* {props.itemList.map((item, index) =>
+        <Item path={item.path}
+          name={item.name}
+          description={item.description}
+          price={item.price}
+          quantity={item.quantity}
+          key={index} />
+      )} */}
     </React.Fragment>
   )
 }
